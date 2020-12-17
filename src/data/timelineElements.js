75adf05cad379553c5d8405d985c47080c3d01ab
Tim <tim.langner@tobit.com>
2020-12-17 16:22:51 +0100

    :sparkles: Add simple data for timeline elements

---
+++ src/data/timelineElements.js
@@ -0,0 +1,62 @@
+let timelineElements = [
+    {
+        id: 1,
+        title: "Training as an IT specialist for application development",
+        location: "Ahaus, Germany",
+        description:
+            "Converting data to a graphical interface, through the use of HTML, CSS, and JavaScript, so that users can view and interact with that data.",
+        buttonText: "View Projects",
+        date: "August 2019 - present",
+        icon: "work",
+    },
+    {
+        id: 2,
+        title: "Internship as a frontend developer",
+        location: "Ahaus, Germany",
+        description:
+            "Working hand-in-hand with front-end developers by providing the outward facing web application elements server-side logic. Creating the logic to make the web app function properly, and accomplishing this through the use of server-side scripting languages.",
+        buttonText: "View Projects",
+        date: "Oktober 2018",
+        icon: "work",
+    },
+    {
+        id: 3,
+        title: "School-based education as an information technology assistant",
+        location: "Nordhorn, Germany",
+        description:
+            "Assessing the quality of specifications and technical design documents in order to ensure timely, relevant and meaningful feedback.",
+        buttonText: "Certificate of completion",
+        date: "August 2017 - June 2019",
+        icon: "school",
+    },
+    {
+        id: 4,
+        title: "Ludwig-Povel-Realschule",
+        location: "Nordhorn, Germany",
+        description:
+            "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        buttonText: "School-leaving certificate",
+        date: "January 2014 - June 2017",
+        icon: "school",
+    },
+    {
+        id: 5,
+        title: "Gymnasium Nordhorn",
+        location: "Nordhorn, Germany",
+        description:
+            "Highschool - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        date: "2010 - January 2014",
+        icon: "school",
+    },
+    {
+        id: 6,
+        title: "Grundschule Brandlecht",
+        location: "Nordhorn, Germany",
+        description:
+            "Elementary school - Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec scelerisque sagittis tellus, non ultrices lacus tempus vel.",
+        date: "2006 - 2010",
+        icon: "school",
+    },
+]
+
+export default timelineElements
